063f4c7ef4eee9c2535ab5430cb66980b9fa52e7
Gary Ong <garyong.ahs@gmail.com>
2019-12-09 15:32:20 +0800

    some changes

--- mysql/mysqldbsetup.sh
+++ mysql/mysqldbsetup.sh
@@ -1,6 +1,6 @@
 echo "downloading data"
 wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/kindle-reviews.zip -O kindle-reviews.zip
-sudo apt install unzip
+sudo apt-get install unzip
 unzip kindle-reviews.zip
 rm -rf kindle_reviews.json
 rm -rf *.zip
